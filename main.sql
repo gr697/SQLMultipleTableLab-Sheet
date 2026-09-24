98d349236897be0fac4e789f2f55ac37e761c412
Gareth Roberts <gr697@bath.ac.uk>
2026-09-24 20:41:09 +0000

    Your commit message

--- main.sql
+++ main.sql
@@ -1,6 +1,1 @@
-INSERT INTO department(Management,Books,Clothes,
-Equipment,Furniture,Navigation,Recreation,Accounting,Purchasing,
-Personnel,Marketing) 
-VALUES(5,1,2,3,4,1,2,5,5,5,5),
-(34,81,24,57,14,41,29,35,36,37,38),
-(1,4,4,3,3,3,4,5,7,9,2);
+SELECT * FROM Employee;
